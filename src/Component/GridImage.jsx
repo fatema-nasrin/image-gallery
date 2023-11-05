20faf990ef5eaff8b2c303521c98273bbd84f5ab
fatema-nasrin <nasrinfatema35@gmail.com>
2023-11-05 18:05:26 +0600

    draggable function added

--- src/Component/GridImage.jsx
+++ src/Component/GridImage.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { TypeAnimation } from "react-type-animation";
+
+const headingStyle = {
+  position: "relative",
+  fontSize: "24px",
+  color: "#004225",
+  fontWeight: "bold",
+  textTransform: "uppercase",
+  textAlign: "center",
+};
+
+const afterStyle = {
+  content: '""',
+  position: "absolute",
+  width: "100%",
+  height: "2px",
+  background: "linear-gradient(to right, transparent, #005000, transparent)",
+  bottom: "0",
+  left: "0",
+};
+
 const GridImage = ({ images }) => {
+  const [imageOrder, setImageOrder] = useState(images);
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const sourceIndex = result.source.index;
+    const destinationIndex = result.destination.index;
+
+    if (sourceIndex !== 11 && destinationIndex !== 11) {
+      const items = Array.from(imageOrder);
+      const [reorderedItem] = items.splice(sourceIndex, 1);
+      items.splice(destinationIndex, 0, reorderedItem);
+      setImageOrder(items);
+    }
+  };
   return (
     <div
       style={{
@@ -6,44 +44,72 @@ const GridImage = ({ images }) => {
         padding: "20px",
         borderRadius: "10px",
         boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-        margin:"20px"
+        margin: "20px",
       }}
     >
-      <div>
-        <h1>Gallery</h1>
+      <div style={headingStyle}>
+        <h1>
+          <TypeAnimation
+            sequence={[
+              "GA",
+              500,
+              "GALL",
+              500,
+              "GALLERY",
+              500,
+              "GALLE",
+              500,
+              "GA",
+              500,
+              "",
+              500,
+            ]}
+            repeat={Infinity}
+          />
+        </h1>
+        <div style={afterStyle}></div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-10">
-        {images &&
-          images.map((imageUrl, index) => (
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
             <div
-              key={index}
-              className={`relative ${
-                index === 0
-                  ? "sm:col-span-2 sm:row-span-2"
-                  : "sm:col-span-1 sm:row-span-1"
-              } flex items-center justify-center `}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 p-10"
             >
-              <div
-                style={{
-                  borderRadius: "10px",
-                  border: index === 11 ? "2px dashed " : "none",
-                }}
-                className={`absolute inset-0 ${index !== 11 ? "transition duration-200 ease-in hover:bg-blue-950 opacity-30" : ""}`}
-              ></div>
-
-              <img
-                src={imageUrl}
-                style={{ borderRadius: "10px" }}
-                className={
-                  `w-full h-auto  ${index === 11 ? "sm:w-1/2 md:w-1/4" : ""} ` &&
-                  `${
-                    index === 11 ? "p-0 py-20 sm:p-0 md:p-0 border-none" : ""
-                  } border-2 border-slate-400`
-                }
-              />
+              {images.map((image, index) => (
+              <Draggable key={image.id} draggableId={image.id} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={{
+                      ...provided.draggableProps.style,
+                      transition: snapshot.isDragging
+                        ? 'transform 0.2s ease'
+                        : 'inherit',
+                    }}
+                    className={`relative ${
+                      index === 0
+                        ? "sm:col-span-2 sm:row-span-2"
+                        : "sm:col-span-1 sm:row-span-1"
+                    } flex items-center justify-center `}
+                  >
+                    <div
+                      
+                    >
+                      <img src={image.url}  className="w-1/2 h-1/2" />
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+              {provided.placeholder}
             </div>
-          ))}
-      </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
--- src/Component/ImageGallery.jsx
+++ src/Component/ImageGallery.jsx
@@ -1,32 +1,32 @@
 import GridImage from "./GridImage";
 
+
 const ImageGallery = () => {
   const images = [
-    "https://i.ibb.co/bmCzn2P/image-1.webp",
-    
-    "https://i.ibb.co/XWXD6cS/image-2.webp",
+    { id: "1", url: "https://i.ibb.co/bmCzn2P/image-1.webp" },
+    { id: "2", url: "https://i.ibb.co/XWXD6cS/image-2.webp" },
 
-    "https://i.ibb.co/Nx3VwjK/image-3.webp",
+    { id: "3", url: "https://i.ibb.co/Nx3VwjK/image-3.webp" },
 
-    "https://i.ibb.co/ZY52vsM/image-4.webp",
+    { id: "4", url: "https://i.ibb.co/ZY52vsM/image-4.webp" },
 
-    "https://i.ibb.co/4SdcmMb/image-5.webp",
+    { id: "5", url: "https://i.ibb.co/4SdcmMb/image-5.webp" },
 
-    "https://i.ibb.co/2srYwFb/image-6.webp",
+    { id: "6", url: "https://i.ibb.co/2srYwFb/image-6.webp" },
 
-    "https://i.ibb.co/D4VHdbS/image-7.webp",
+    { id: "7", url: "https://i.ibb.co/D4VHdbS/image-7.webp" },
 
-    "https://i.ibb.co/XyQYMLP/image-8.webp",
+    { id: "8", url: "https://i.ibb.co/XyQYMLP/image-8.webp" },
 
-    "https://i.ibb.co/JHKcH8x/image-9.webp",
+    { id: "9", url: "https://i.ibb.co/JHKcH8x/image-9.webp" },
 
-    "https://i.ibb.co/jJ5WX5v/image-10.jpg",
+    { id: "10", url: "https://i.ibb.co/jJ5WX5v/image-10.jpg" },
 
-    "https://i.ibb.co/JqdqNN4/image-11.jpg",
-    
-    "/src/assets/icons8-image.gif"
+    { id: "11", url: "https://i.ibb.co/JqdqNN4/image-11.jpg" },
+
+    { id: "12", url: "/src/assets/icons8-image.gif" },
   ];
-  return <GridImage images={images} /> ;
+  return <GridImage images={images} />;
 };
 
 export default ImageGallery;
